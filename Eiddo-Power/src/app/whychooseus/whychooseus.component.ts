039aa83664906a5661d5d804143693e75991fedc
iamfahad948 <fahadullah948@gmail.com>
2025-01-30 10:47:11 +0530

    wcu page

---
+++ Eiddo-Power/src/app/whychooseus/whychooseus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whychooseus',
+  standalone: false,
+  
+  templateUrl: './whychooseus.component.html',
+  styleUrl: './whychooseus.component.css'
+})
+export class WhychooseusComponent {
+
+}
